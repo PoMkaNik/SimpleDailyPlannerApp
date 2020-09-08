9db8ef39564d993bdac3fc31864ecbab1493c6a1
Roman Dubovik <ip.romandubovik@gmail.com>
2020-09-08 11:49:32 +0300

    app 1.1 removed some unnecessary nesting

--- app.js
+++ app.js
@@ -186,18 +186,15 @@ function showOneTask(task) {
     return task.taskDate.slice(2).split('-').reverse().join('/');
   }
 
-  return `<div class="row py-1 m-0">
-  <div class="col-1 px-1 align-self-center">
-    <div class="d-flex align-self-center">
+  return `<div class="row align-items-center py-1 m-0">
+  <div class="col-1 px-1 d-flex align-items-center">
     <!-- Priority icon -->
     ${priorityIcon(task)}
-    </div>
   </div>
   <div class="col px-1 taskTitle">${task.taskName}</div>
 </div>
-<div class="row py-1 m-0">
-  <div class="col px-1 align-self-center">
-    <div class="d-flex align-self-center">
+<div class="row align-items-center py-1 m-0">
+  <div class="col px-1 d-flex align-items-center">
       <svg
         width="1.5em"
         height="1.5em"
@@ -214,10 +211,8 @@ function showOneTask(task) {
           d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"
         /></svg
       ><span class="px-1">${fixDatePresentation(task)}</span>
-    </div>
   </div>
-  <div class="col px-1 align-self-center">
-    <div class="d-flex align-self-center">
+  <div class="col px-1 d-flex align-items-center">
       <svg
         width="1.5em"
         height="1.5em"
@@ -231,7 +226,6 @@ function showOneTask(task) {
           d="M6.5 0a.5.5 0 0 0 0 1H7v1.07a7.001 7.001 0 0 0-3.273 12.474l-.602.602a.5.5 0 0 0 .707.708l.746-.746A6.97 6.97 0 0 0 8 16a6.97 6.97 0 0 0 3.422-.892l.746.746a.5.5 0 0 0 .707-.708l-.601-.602A7.001 7.001 0 0 0 9 2.07V1h.5a.5.5 0 0 0 0-1h-3zm1.038 3.018a6.093 6.093 0 0 1 .924 0 6 6 0 1 1-.924 0zM8.5 5.5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM0 3.5c0 .753.333 1.429.86 1.887A8.035 8.035 0 0 1 4.387 1.86 2.5 2.5 0 0 0 0 3.5zM13.5 1c-.753 0-1.429.333-1.887.86a8.035 8.035 0 0 1 3.527 3.527A2.5 2.5 0 0 0 13.5 1z"
         /></svg
       ><span class="px-1">${task.taskTime}</span>
-    </div>
   </div>
   <div class="col px-1">
     <button type="button" class="btn btn-info btn-block" onclick="location.href='editTask.html?id=${
